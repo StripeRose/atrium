741728787d79d302fe1e199dab56e999a9931bb1
Stripe Rose <stripe@striperose.se>
2023-12-08 23:49:07 +0100

    Better rose-common project setup.

--- code/libraries/rose-common.sharpmake.cs
+++ code/libraries/rose-common.sharpmake.cs
@@ -9,6 +9,15 @@ public class RoseCommon : RoseGold.Core.Library
     public RoseCommon()
     {
         Name = "rose-common";
-        SourceRootPath = Path.Combine(Globals.RootDirectory, "libraries");
+        SourceRootPath = Path.Combine(Globals.RootDirectory, "libraries/rose-common");
+
+        SourceFilesExcludeRegex.Add(@".*notready.*");
+    }
+
+    public override void ConfigureAll(Configuration conf, Target target)
+    {
+        base.ConfigureAll(conf, target);
+
+        conf.ProjectPath = Path.Combine(Globals.RootDirectory, "libraries");
     }
 }
