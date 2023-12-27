7a67e28364f79956a622260913394fd59b103e0b
Stripe Rose <stripe@striperose.se>
2023-12-27 15:41:30 +0100

    Remove empty window, increase framerate.

--- code/example/Program.cpp
+++ code/example/Program.cpp
@@ -14,7 +14,7 @@
 #include <chrono>
 
 std::chrono::high_resolution_clock::time_point ourStartTime;
-std::shared_ptr<RoseGold::Core::Graphics::RenderTexture> ourRT1, ourRT2, ourRT3;
+std::shared_ptr<RoseGold::Core::Graphics::RenderTexture> ourRT1, ourRT2;
 
 std::shared_ptr<RoseGold::Core::Graphics::CachedPipelineState> ourMeshPipelineState;
 std::shared_ptr<RoseGold::Core::Graphics::Shader> ourVertexShader, ourPixelShader;
@@ -26,8 +26,7 @@ void SetupResources(RoseGold::Client::BootstrapResult& roseGold)
 	RoseGold::Core::Platform::WindowManager::CreationParameters windowParams;
 
 	windowParams.Title = "Window 1";
-	windowParams.Position = { 200, 200 };
-	windowParams.Size = { 200, 200 };
+	windowParams.Size = { 640, 480 };
 	auto window1 = roseGold.WindowManager->NewWindow(windowParams);
 	ourRT1 = roseGold.GraphicsManager->CreateRenderTextureForWindow(*window1);
 	window1->Closed.Connect(nullptr, [&](RoseGold::Core::Platform::Window&) {
@@ -35,34 +34,12 @@ void SetupResources(RoseGold::Client::BootstrapResult& roseGold)
 		});
 
 	windowParams.Title = "Window 2";
-	windowParams.Position = { 410, 200 };
-	windowParams.Size = { 400, 200 };
+	windowParams.Size = { 640, 480 };
 	auto window2 = roseGold.WindowManager->NewWindow(windowParams);
 	ourRT2 = roseGold.GraphicsManager->CreateRenderTextureForWindow(*window2);
 	window2->Closed.Connect(nullptr, [&](RoseGold::Core::Platform::Window&) {
 		ourRT2.reset();
 		});
-
-	windowParams.Title = "Window 3";
-	windowParams.Position = { 200, 440 };
-	windowParams.Size = { 200, 400 };
-	auto window3 = roseGold.WindowManager->NewWindow(windowParams);
-	ourRT3 = roseGold.GraphicsManager->CreateRenderTextureForWindow(*window3);
-	window3->Closed.Connect(nullptr, [&](RoseGold::Core::Platform::Window&) {
-		ourRT3.reset();
-		});
-
-	auto onWindowRectChange = [window2, window3](RoseGold::Core::Platform::Window& aWindow) {
-		const RoseGold::Point point = aWindow.GetPosition();
-		const RoseGold::Size size = aWindow.GetSize();
-		const RoseGold::Point bottomRight = point + size;
-
-		window2->SetPosition({ bottomRight.X + 10, point.Y });
-		window3->SetPosition({ point.X, bottomRight.Y + 40 });
-	};
-
-	window1->Moved		.Connect(nullptr, onWindowRectChange);
-	window1->SizeChanged.Connect(nullptr, onWindowRectChange);
 
 	{
 		ourMesh1 = roseGold.GraphicsManager->CreateMesh();
@@ -101,7 +78,6 @@ void CleanupResources()
 	ourMesh3.reset();
 	ourRT1.reset();
 	ourRT2.reset();
-	ourRT3.reset();
 }
 
 void DrawFrame(RoseGold::Core::Graphics::Manager& aManager)
@@ -109,7 +85,6 @@ void DrawFrame(RoseGold::Core::Graphics::Manager& aManager)
 	const std::chrono::high_resolution_clock::duration timeSinceStart = (std::chrono::high_resolution_clock::now() - ourStartTime);
 	const std::chrono::milliseconds msSinceStart = std::chrono::duration_cast<std::chrono::milliseconds>(timeSinceStart);
 	const float secondsSinceStart = msSinceStart.count() / 1000.f;
-	const float lerp = (RoseCommon::Math::Sine<float>(secondsSinceStart) + 1.f) / 2.f;
 
 	using namespace RoseGold::Core::Graphics;
 	GraphicsTask drawFrame;
@@ -159,33 +134,14 @@ void DrawFrame(RoseGold::Core::Graphics::Manager& aManager)
 		{
 			CommandBuffer& buffer = clearWindow1.AddWork();
 			buffer.SetRenderTarget(ourRT1);
-			buffer.Clear(RoseCommon::Math::Lerp<RoseGold::Color>(
-				RoseGold::Color::Predefined::Aqua,
-				RoseGold::Color::Predefined::Tan,
-				lerp
-			), 1.f);
+			buffer.Clear(RoseGold::Color::Predefined::CornflowerBlue, 1.f);
 		}
 
 		GraphicsTask& clearWindow2 = clearTask.CreateTask("Clear window 2");
 		{
 			CommandBuffer& buffer = clearWindow2.AddWork();
 			buffer.SetRenderTarget(ourRT2);
-			buffer.Clear(RoseCommon::Math::Lerp<RoseGold::Color>(
-				RoseGold::Color::Predefined::Tomato,
-				RoseGold::Color::Predefined::LightGreen,
-				lerp
-			), 1.f);
-		}
-
-		GraphicsTask& clearWindow3 = clearTask.CreateTask("Clear window 3");
-		{
-			CommandBuffer& buffer = clearWindow3.AddWork();
-			buffer.SetRenderTarget(ourRT3);
-			buffer.Clear(RoseCommon::Math::Lerp<RoseGold::Color>(
-				RoseGold::Color::Predefined::LightYellow,
-				RoseGold::Color::Predefined::CornflowerBlue,
-				lerp
-			), 1.f);
+			buffer.Clear(RoseGold::Color::Predefined::Black, 1.f);
 		}
 	}
 
@@ -219,7 +175,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR someCommandArg
 		DrawFrame(*roseGold.GraphicsManager);
 		roseGold.GraphicsManager->MarkFrameEnd();
 
-		std::this_thread::sleep_until(frameStart + std::chrono::milliseconds(16));
+		std::this_thread::sleep_until(frameStart + std::chrono::milliseconds(8));
 	}
 
 	CleanupResources();
