fc5a71887ee34e9673846e642a81cd7f17b78968
Stripe Rose <stripe@striperose.se>
2023-12-27 16:33:38 +0100

    Simple test lighting in shader.

--- data/shaders/MeshPixel.hlsl
+++ data/shaders/MeshPixel.hlsl
@@ -1,16 +1,28 @@
 struct PixelInput
 {
-    float4 Position : SV_Position;
-    float3 Normal : NORMAL;
-    float2 UV : TEXCOORD;
-    float3 Color : COLOR;
+    float4 ScreenPosition : SV_Position;
+    float3 WorldPosition : POSITION0;
+    float2 ScreenCoordinate : POSITION1;
     
+    float3 Normal : NORMAL;
     float3 Binormal : BINORMAL;
     float3 Tangent : TANGENT;
+    
+    float2 UV : TEXCOORD;
+    float3 Color : COLOR;
 };
 
 float4 main(PixelInput anInput) : SV_TARGET
 {
-    return float4(anInput.Color, 1.0f);
-
+    const float3 skyColor = (float3(4, 135, 226) / 255) * 0.8f;
+    const float3 sunColor = (float3(226, 82, 4) / 255);
+    const float3 ambientColor = float3(0.5f, 0.5f, 0.5f);
+    
+    const float sunDiffuse = saturate(dot(anInput.Normal, normalize(float3(0, 1, -1))));
+    const float skyDiffuse = saturate((dot(anInput.Normal, float3(0, 1, 0)) * 0.5f) + 0.5f);
+    const float ambientLight = 0.1f;
+    
+    const float3 totalColor = saturate((skyColor * skyDiffuse) + (sunColor * sunDiffuse) + (ambientColor * ambientLight));
+    
+    return float4(totalColor, 1.0f);
 }
--- data/shaders/MeshVertex.hlsl
+++ data/shaders/MeshVertex.hlsl
@@ -18,28 +18,44 @@ struct VertexInput
 
 struct VertexOutput
 {
-    float4 Position : SV_Position;
-    float3 Normal : NORMAL;
-    float2 UV : TEXCOORD;
-    float3 Color : COLOR;
+    float4 ScreenPosition : SV_Position;
+    float3 WorldPosition : POSITION0;
+    float2 ScreenCoordinate : POSITION1;
     
+    float3 Normal : NORMAL;
     float3 Binormal : BINORMAL;
     float3 Tangent : TANGENT;
+    
+    float2 UV : TEXCOORD;
+    float3 Color : COLOR;
 };
 
 VertexOutput main(VertexInput anInput)
 {
-    float4x4 modelViewProjection = mul(ModelMatrix, mul(ViewMatrix, ProjectionMatrix));
+    const float4 worldPosition = mul(float4(anInput.Position, 1.0f), ModelMatrix);
+    const float4 cameraSpacePosition = mul(worldPosition, ViewMatrix);
+    const float4 projectionPosition = mul(cameraSpacePosition, ProjectionMatrix);
+    
+    const float3 worldNormal = normalize(float3(mul(float4(anInput.Normal, 0), ModelMatrix).xyz));
+    const float3 worldBiNormal = normalize(float3(mul(float4(anInput.Binormal, 0), ModelMatrix).xyz));
+    const float3 worldTangentNormal = normalize(float3(mul(float4(anInput.Tangent, 0), ModelMatrix).xyz));
     
     VertexOutput output;
-    output.Position = mul(float4(anInput.Position, 1.0f), modelViewProjection);
-    output.Position /= output.Position.w;
-    output.Normal = anInput.Normal;
+    
+    output.WorldPosition = worldPosition.xyz;
+    output.ScreenPosition = projectionPosition / projectionPosition.w;
+    
+    output.ScreenCoordinate = output.ScreenPosition.xy;
+    output.ScreenCoordinate.y *= -1;
+    output.ScreenCoordinate /= 2;
+    output.ScreenCoordinate += 0.5;
+    
+    output.Normal = worldNormal;
+    output.Binormal = worldBiNormal;
+    output.Tangent = worldTangentNormal;
+    
     output.UV = anInput.UV;
     output.Color = anInput.Color;
-    
-    output.Binormal = anInput.Binormal;
-    output.Tangent = anInput.Tangent;
     
     return output;
 }
