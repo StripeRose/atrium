c9208833b8b7c5487e100a3f55fce86eafd649f4
Stripe Rose <stripe@striperose.se>
2025-05-18 20:01:12 +0200

    Reworking engine instance for inheritance Inheritance will (hopefully) ensure that the derived code are all released before the engine itself is released by the parent class. This should clean up the code a bit for the users. Next step is to provide static functions for creating resources.

---
+++ engine/Atrium.cpp
@@ -0,0 +1,144 @@
+#include "Atrium.hpp"
+
+#include "Atrium_AudioAPI.hpp"
+#include "Atrium_Diagnostics.hpp"
+#include "Atrium_GraphicsAPI.hpp"
+#include "Atrium_InputDeviceAPI.hpp"
+#include "Atrium_WindowManagement.hpp"
+
+namespace Atrium
+{
+	AtriumApplication::AtriumApplication()
+		: myIsRunning(false)
+		, myHasShutdownBeenRequested(false)
+	{
+		CreateAPIHandlers();
+		AssertAPIHandlersExist();
+
+		SetupEngine();
+	}
+
+	AtriumApplication::~AtriumApplication()
+	{
+		// Clean up is handled in the destructor instead of before Run() returns,
+		// to be able to rely on the derived class to have released all its objects first.
+
+		CleanupEngine();
+	}
+
+	void AtriumApplication::Exit()
+	{
+		myIsRunning = false;
+	}
+
+	bool AtriumApplication::Run()
+	{
+		if (!HandleStartup())
+			return false;
+
+		myIsRunning = true;
+
+		RunMainLoop();
+
+		HandleShutdown();
+
+		return true;
+	}
+
+	void AtriumApplication::HandleCloseRequest(bool&)
+	{
+		// The default implementation will always honor the request to close the application and not change the request.
+		// We don't want to risk the application being unable to exit.
+	}
+
+	void AtriumApplication::SetupEngine()
+	{
+		// Core systems have been set up and are usable.
+		// Prepare all sub-systems for use.
+
+		// Todo: Assert there is no other application instance created on the thread previously.
+
+		// Todo: Prepare sub-system.
+	}
+
+	void AtriumApplication::RunMainLoop()
+	{
+		while (myIsRunning)
+		{
+			if (myHasShutdownBeenRequested)
+			{
+				HandleCloseRequest(myHasShutdownBeenRequested);
+				myIsRunning = !myHasShutdownBeenRequested;
+				myHasShutdownBeenRequested = false;
+				continue;
+			}
+
+			DoTick();
+		}
+	}
+
+	void AtriumApplication::DoTick()
+	{
+		myWindowManager->Update();
+
+		myInputDeviceAPI->ReportInputEvents(~Core::InputDeviceType::Unknown);
+
+		myGraphicsAPI->MarkFrameStart();
+
+		HandleFrameLogic();
+
+		myGraphicsAPI->MarkFrameEnd();
+	}
+
+	void AtriumApplication::CleanupEngine()
+	{
+		// Game has exited and cleaned up its data, and is expected to not use the sub-systems any more.
+		// Time to clean them up before the run finally ends.
+
+		// Todo: Clean up the sub-systems.
+	}
+}
+
+//void EngineInstance::InitializeImGui(Window& aPrimaryWindow, std::shared_ptr<RenderTexture> aTarget)
+//{
+//	Debug::Assert(!myImGuiHandler, "Only one primary ImGui window supported.");
+//	myImGuiHandler.reset(new ImGuiHandler(*myGraphicsAPI, aPrimaryWindow, aTarget));
+//	aPrimaryWindow.Closed.Connect(this, [&](Window&) { myImGuiHandler.reset(); });
+//}
+//
+//void EngineInstance::Run()
+//{
+//	myIsRunning = true;
+//	OnStart.Invoke();
+//
+//	while (myIsRunning)
+//	{
+//		myWindowManager->Update();
+//
+//		myGraphicsAPI->MarkFrameStart();
+//
+//		InputDeviceType allowedInputDevices = ~InputDeviceType::Unknown;
+//
+//		if (myImGuiHandler)
+//		{
+//			myImGuiHandler->MarkFrameStart();
+//			allowedInputDevices = myImGuiHandler->GetAllowedInputs();
+//		}
+//
+//		myInputDeviceAPI->ReportInputEvents(allowedInputDevices);
+//
+//		OnLoop.Invoke();
+//
+//		if (myImGuiHandler)
+//		{
+//			OnImGui.Invoke();
+//			myImGuiHandler->MarkFrameEnd();
+//		}
+//
+//		myGraphicsAPI->MarkFrameEnd();
+//	}
+//
+//	myImGuiHandler.reset();
+//
+//	OnExit.Invoke();
+//}
---
+++ engine/Atrium_Create.cpp
@@ -0,0 +1,54 @@
+#include "Atrium.hpp"
+
+#include "Atrium_Diagnostics.hpp"
+
+#include "Atrium_NullAudioHandler.hpp"
+#include "Atrium_NullGraphicsHandler.hpp"
+#include "Atrium_NullInputHandler.hpp"
+#include "Atrium_NullWindowHandler.hpp"
+
+#if _WIN32
+#include "DX12_Instancer.hpp"
+#include "Win32_WindowManagement.hpp"
+#include "Win32_InputDeviceAPI.hpp"
+#endif
+
+namespace Atrium
+{
+	// This function sets up all core API handlers that the rest of the engine is building off of, depending on the current platform.
+	void AtriumApplication::CreateAPIHandlers()
+	{
+		ZoneScoped;
+
+		#if _WIN32
+
+		myGraphicsAPI.reset(DirectX12::CreateDX12Manager().release());
+		myInputDeviceAPI.reset(new Win32::InputDeviceAPI());
+		myWindowManager.reset(new Win32::WindowManager());
+
+		#elif !defined(IGNORE_NOOP_PLATFORM)
+
+		Debug::LogFatal(
+			"This platform deos not have any core implementations. Atrium will not do anything.\n"
+			"If this is intentional, define \"IGNORE_NOOP_PLATFORM\" to disable this error."
+		);
+
+		#endif
+
+		// Populate null objects for missing API handlers.
+		if (!myAudioAPI) myAudioAPI.reset(new Core::NullAudioHandler());
+		if (!myInputDeviceAPI) myInputDeviceAPI.reset(new Core::NullInputHandler());
+		if (!myGraphicsAPI) myGraphicsAPI.reset(new Core::NullGraphicsHandler());
+		if (!myWindowManager) myWindowManager.reset(new Core::NullWindowManager());
+	}
+
+	void AtriumApplication::AssertAPIHandlersExist()
+	{
+		// To avoid needing null-checks later, these basic systems must have an instance of some kind of handler.
+		// If the functionality isn't desired or isn't supported on a particular platform, they should have null objects populated.
+		Debug::Assert(!!myAudioAPI, "Application is expected to have an audio API handler.");
+		Debug::Assert(!!myGraphicsAPI, "Application is expected to have a graphics API handler.");
+		Debug::Assert(!!myInputDeviceAPI, "Application is expected to have an input device API handler.");
+		Debug::Assert(!!myWindowManager, "Application is expected to have a window API handler.");
+	}
+}
