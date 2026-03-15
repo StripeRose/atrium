36a282267833f0938e95f834f798345b6a931d90
Stripe Rose <stripe@striperose.se>
2026-03-15 13:41:39 +0100

    Add more Tracy logging

--- core/Atrium_Diagnostics.cpp
+++ core/Atrium_Diagnostics.cpp
@@ -20,4 +20,89 @@ void operator delete(void* ptr) noexcept
 	TracyFree(ptr);
 	free(ptr);
 }
+
+class TracyDebugLog final : public Atrium::Debug::Impl
+{
+public:
+	static void Attach()
+	{
+		static TracyDebugLog instance;
+		instance.ourParentHandler = Atrium::Debug::ourDebugImplementation;
+		Atrium::Debug::ourDebugImplementation = &instance;
+	}
+
+	static void Detach()
+	{
+		TracyDebugLog& instance = static_cast<TracyDebugLog&>(*Atrium::Debug::ourDebugImplementation);
+
+		Atrium::Debug::ourDebugImplementation = instance.ourParentHandler;
+		instance.ourParentHandler = nullptr;
+	}
+
+	void Log(LogType aType, const std::string& aMessage) override
+	{
+		tracy::MessageSeverity severity = tracy::MessageSeverity::Info;
+		uint32_t color = 0;
+		switch (aType)
+		{
+			case LogType::Information:
+				severity = tracy::MessageSeverity::Info;
+				color = 0x8080FF;
+				break;
+			case LogType::Warning:
+				severity = tracy::MessageSeverity::Warning;
+				color = 0xFFFF80;
+				break;
+			case LogType::Error:
+				severity = tracy::MessageSeverity::Error;
+				color = 0xFF8080;
+				break;
+			case LogType::Fatal:
+				severity = tracy::MessageSeverity::Fatal;
+				color = 0xFF0000;
+				break;
+		}
+
+		TracyLogString(severity, color, TRACY_CALLSTACK, aMessage.size(), aMessage.c_str());
+		TracyDebugLog::ourParentHandler->Log(aType, aMessage);
+	}
+
+	void Log(LogType aType, const std::wstring& aMessage) override
+	{
+		TracyDebugLog::ourParentHandler->Log(aType, aMessage);
+	}
+
+	void SetThreadName(const std::string& aThreadName) override
+	{
+		TracyDebugLog::ourParentHandler->SetThreadName(aThreadName);
+	}
+
+	void SetThreadName(std::thread& aThread, const std::string& aThreadName) override
+	{
+		TracyDebugLog::ourParentHandler->SetThreadName(aThread, aThreadName);
+	}
+
+	void TriggerCrash(const std::string& aMessage) override
+	{
+		TracyDebugLog::ourParentHandler->TriggerCrash(aMessage);
+	}
+
+	void TriggerCrash(const std::wstring& aMessage) override
+	{
+		TracyDebugLog::ourParentHandler->TriggerCrash(aMessage);
+	}
+
+private:
+	Atrium::Debug::Impl* ourParentHandler;
+};
+
+void TracyDebug_AttachLog()
+{
+	TracyDebugLog::Attach();
+}
+
+void TracyDebug_DetachLog()
+{
+	TracyDebugLog::Detach();
+}
 #endif
--- core/Atrium_Diagnostics.hpp
+++ core/Atrium_Diagnostics.hpp
@@ -15,18 +15,46 @@ _VCRT_ALLOCATOR
 void* operator new(std::size_t count) noexcept(false);
 void operator delete(void* ptr) noexcept;
 
+void TracyDebug_AttachLog();
+void TracyDebug_DetachLog();
+
+#define PROFILE_ATTACH_LOG() TracyDebug_AttachLog()
+#define PROFILE_DETACH_LOG() TracyDebug_DetachLog()
+
+#define PROFILE_APPINFO(aText) TracyAppInfo(aText, std::strlen(aText))
+
 #define PROFILE_FRAMEMARK() FrameMark
+#define PROFILE_FRAMEMARK_NAMED(aName) FrameMarkNamed(aName)
+
 #define PROFILE_SCOPE() ZoneScoped
 #define PROFILE_SCOPE_NAME(aName) ZoneScopedN(aName)
 #define PROFILE_SCOPE_COLOR(anARGB) ZoneScoped; ZoneColor(0x00FFFFFF & anARGB)
-#define PROFILE_SCOPE_NAME_COLOR(aName, anARGB) ZoneScopedN(aName); ZoneColor(0x00FFFFFF & anARGB)
+#define PROFILE_SCOPE_NAME_COLOR(aName, anARGB) ZoneScopedNC(aName, 0x00FFFFFF & anARGB)
+
+#define PROFILE_ACTIVE_SCOPE_TEXT(aFormat, ...) ZoneTextF(aFormat, ##__VA_ARGS__)
+#define PROFILE_ACTIVE_SCOPE_NAME(aFormat, ...) ZoneNameF(aFormat, ##__VA_ARGS__)
+#define PROFILE_ACTIVE_SCOPE_COLOR(aColor) ZoneColor(0x00FFFFFF & anARGB)
+#define PROFILE_ACTIVE_SCOPE_VALUE(aValue) ZoneValue(aValue)
+
+#define PROFILE_PLOT(aName, aValue) TracyPlot(aName, aValue)
 
 #else
 
+#define PROFILE_APPINFO(aText)
+
 #define PROFILE_FRAMEMARK()
+#define PROFILE_FRAMEMARK_NAMED(aName)
+
 #define PROFILE_SCOPE()
 #define PROFILE_SCOPE_NAME(aName)
 #define PROFILE_SCOPE_COLOR(anARGB)
 #define PROFILE_SCOPE_NAME_COLOR(aName, anARGB)
+
+#define PROFILE_ACTIVE_SCOPE_TEXT(aFormat, ...)
+#define PROFILE_ACTIVE_SCOPE_NAME(aFormat, ...)
+#define PROFILE_ACTIVE_SCOPE_COLOR(aColor)
+#define PROFILE_ACTIVE_SCOPE_VALUE(aValue)
+
+#define PROFILE_PLOT(aName, aValue)
 
 #endif
